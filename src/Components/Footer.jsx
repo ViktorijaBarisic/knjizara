1432dae244bd07e054d91fe4d7f634c089577f84
Viktorija Barisic <vbarisic511@gmail.com>
2023-01-21 22:25:20 +0100

    Napravljen izgled pocetne strane

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <p1> Svet knjiga </p1>
+    </div>
+  )
+}
+
+export default Footer
+
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    
+        <div className="navbar navbar-custom header"  >
+            <a className="navbar-brand" href="">
+            Knjižara <strong>
+                    Knjige
+                </strong>
+            </a> 
+            <div>
+            <a className="nav-link" href=""> Početna </a>
+            <a className="nav-link" href=""> Dodaj novu knjigu </a>
+           
+    
+            </div>
+        
+        </div>
+
+   
+  )
+}
+
+export default Navbar
